80ab5375318113ca77cdc5036ff465f73a2c6355
Gregory Bouteiller <gregory.bouteiller@gmail.com>
2024-06-09 19:58:33 +0900

    up

---
+++ astro.config.mjs
@@ -0,0 +1,16 @@
+import alpinejs from "@astrojs/alpinejs"
+import netlify from "@astrojs/netlify"
+import tailwind from "@astrojs/tailwind"
+import { defineConfig } from "astro/config"
+
+// https://astro.build/config
+export default defineConfig({
+  output: "server",
+  adapter: netlify(),
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    alpinejs({ entrypoint: "/src/lib//alpine" }),
+  ],
+})
